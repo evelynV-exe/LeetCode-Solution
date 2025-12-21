680fb034bbdcd1556a2c1b7f9eef6116e6592d3c
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-12-21 14:55:26 +0700

    Add files via upload

---
+++ Two Sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, input: list[int], target: int) -> list[int]:
+
+        lookup = {}
+        for i, num in enumerate(input):
+            complement = target - num
+            if complement in lookup:
+                return [lookup[complement], i]
+            lookup[num] = i
+
+input = [2, 7, 11, 15]
+target = 9
+
+print(Solution.twoSum(input, target))
