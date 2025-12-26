d430ff60987b7e01fce198990dff15d149026ea3
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-12-26 21:35:45 +0700

    Add files via upload

---
+++ Easy/Merge Sorted Array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        i, j, k = m - 1, n - 1, m + n - 1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+        
+nums1 = [1, 2, 3, 0, 0, 0]
+nums2 = [2, 5, 6]
+m = 3
+n = 3
+
+Solution().merge(nums1, m, nums2, n)
+print(nums1)
